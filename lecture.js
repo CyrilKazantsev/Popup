f38925b4a85cf844245fe8952ff162efbbede110
Cyril Kazantsev <kir.kazancef@yandex.ru>
2022-01-27 00:02:10 +0500

    Первый черновик

---
+++ lecture.js
@@ -0,0 +1,35 @@
+const images = ["images/dog.svg", "images/bird.svg", "images/cat.svg"];
+
+images.forEach(el => {
+    const img = document.createElement("div");
+    img.className = "mini__item";
+    img.style.backgroundImage = `url(${el})`;
+    img.addEventListener("click", e => 
+        showPopup(`<img alt="pic" src="${el}">`)
+    )
+    document.body.append(img);
+})
+
+const popup = document.querySelector(".popup");
+const popupContent = popup.firstElementChild;
+const popupClose = popup.lastElementChild;
+
+const closePopup = function(e) {
+    e.target.parentElement.classList.remove("popup_active");
+}
+const showPopup = function(text) {
+    popup.classList.add("popup_active");
+    popupContent.innerHTML = text;
+}
+
+popupClose.addEventListener("click", closePopup)
+
+// hw1 link(git)
+/* 
+    1) При нажатии на поле вне картинки закрывать popup
+    2) Предусмотреть возможность закрытия окошка по нажатию на кнопки:
+        (keyup/keydown)
+        1. Esc
+        2. Alt+F4 (preventDefault)
+    3) Сделать интерфейс в виде lightbox - все элементы за popup становятся затенёнными
+*/
