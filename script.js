b8168659caa2949629680d2c4069744d8780f1d4
Cyril Kazantsev <kir.kazancef@yandex.ru>
2022-01-27 19:42:33 +0500

    Затенение, Escape, Alt+F4 (в теории), Нажатие вне поля

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const images = ["images/dog.svg", "images/bird.svg", "images/cat.svg"];
 
+// Открытие картинки во всплывающем окне
 images.forEach(el => {
     const img = document.createElement("div");
     img.className = "mini__item";
@@ -13,16 +14,38 @@ images.forEach(el => {
 const popup = document.querySelector(".popup");
 const popupContent = popup.firstElementChild;
 const popupClose = popup.lastElementChild;
+const lightBox = document.querySelector(".lightbox")
 
+// Закрытие всплывающего окна, отключение затенения
 const closePopup = function(e) {
     e.target.parentElement.classList.remove("popup_active");
+    lightBox.classList.remove("lightbox_active")
 }
+
+// Открытие всплывающего окна, затенение остального экрана
 const showPopup = function(text) {
     popup.classList.add("popup_active");
+    lightBox.classList.add("lightbox_active")
     popupContent.innerHTML = text;
 }
 
 popupClose.addEventListener("click", closePopup)
+
+// Event на нажатие "Escape" и сочетания клавиш "Alt+F4"
+document.addEventListener("keydown", function(event){
+    if (event.code === "Escape" || (event.code === "F4" && event.altKey)) { // Alt+F4 отказывается работать
+        event.preventDefault();
+        document.querySelector(".popup_active").classList.remove("popup_active")
+        lightBox.classList.remove("lightbox_active")
+    }
+})
+
+// Закрытие popup при нажатии вне
+lightBox.addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector(".popup_active").classList.remove("popup_active")
+    lightBox.classList.remove("lightbox_active")
+})
 
 // hw1 link(git)
 /* 
